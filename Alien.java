001747f3ec0975fd1b87152a07759798dbb0b060
19481A1281 <nallapaneni.durga3@gmail.com>
2023-11-08 16:08:49 +0530

    program1

---
+++ Alien.java
@@ -0,0 +1,17 @@
+//AlienNumber
+
+class Alien{
+public static void main(String...args){
+int n1=12,sq1=n1*n1,n2=0,sq2=0,sq2rev=0;;
+while(n1>0){
+n2=n2*10+n1%10;
+n1/=10;
+}
+sq2=n2*n2;
+while(sq2>0){
+sq2rev=sq2rev*10+sq2%10;
+sq2/=10;
+}
+System.out.println(sq1==sq2rev?"Alien Number":"Not an Alien Number");
+}
+}
